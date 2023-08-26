fe0ed7f6049cfa10206d563b8bc31a7b3b1d69ea
shina1 <oswasiu@gmail.com>
2023-08-26 15:11:45 +0100

    feat: added eslint rule to code base

--- src/components/modules/Forms/TeachersForm.tsx
+++ src/components/modules/Forms/TeachersForm.tsx
@@ -1,7 +1,80 @@
+import Button from "@/components/elements/Button";
+import InputComponent from "@/components/elements/InputComponent";
 import React from "react";
+import { FormProvider, useForm } from "react-hook-form";
 
 const TeachersForm = () => {
-  return <div></div>;
+  const methods = useForm();
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    console.log("...submit");
+  };
+  return (
+    <FormProvider {...methods}>
+      <div className="px-6 py-6 lg:px-8">
+        <h3 className="mb-4 text-xl font-medium text-gray-900 ">
+          Add a New Teacher
+        </h3>
+        <form className="space-y-6" action="#">
+          <div>
+            <InputComponent
+              name="nationalId"
+              type="text"
+              classNames="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeHolder="Nationa Id number (NIN)"
+              label="National ID Number"
+            />
+          </div>
+          <div>
+            <InputComponent
+              name="name"
+              type="text"
+              classNames="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeHolder="Teachers name"
+              label="Name"
+            />
+          </div>
+          <div>
+            <InputComponent
+              name="surname"
+              type="text"
+              classNames="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeHolder="Teachers surname"
+              label="Surname"
+            />
+          </div>
+          <div>
+            <InputComponent
+              name="dob"
+              type="date"
+              classNames="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeHolder="mm/dd/yy"
+              label="Date of birth"
+            />
+          </div>
+          <div>
+            <InputComponent
+              name="teacherNo"
+              type="number"
+              classNames="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeHolder="Teacher Number"
+              label="Teacher Number"
+            />
+          </div>
+          <div>
+            <InputComponent
+              name="salary"
+              type="text"
+              classNames="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              placeHolder="Teacher's Salary"
+              label="Teacher's Salary"
+            />
+          </div>
+          <Button text="Submit" type="secondary" handleClick={handleSubmit} />
+        </form>
+      </div>
+    </FormProvider>
+  );
 };
 
 export default TeachersForm;
---
+++ src/components/modules/Modals/StudentFormModal.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import StudentForm from "../Forms/StudentForm";
+
+interface compProps {
+  toggleStudent: boolean;
+  setToggleStudent: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const StudentFormModal = ({ setToggleStudent, toggleStudent }: compProps) => {
+  return (
+    <div className="fixed top-5 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full bg-[rgba(65,70,82,0.7)]">
+      <div className="relative w-full h-full max-w-2xl md:h-auto mx-auto md:top-[100px]">
+        <div className="relative bg-white rounded-lg shadow ">
+          <button
+            onClick={() => setToggleStudent(!toggleStudent)}
+            type="button"
+            className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center"
+            data-modal-hide="authentication-modal"
+          >
+            <svg
+              className="w-3 h-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+              />
+            </svg>
+            <span className="sr-only">Close modal</span>
+          </button>
+          <StudentForm />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StudentFormModal;
